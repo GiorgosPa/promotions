d44285dd1317a3208d339fecbf7f4e1a4abec29a
Georgios Papoutsakis <gepapout@microsoft.com>
2021-04-21 19:51:17 +0200

    Add test for registering promotions

--- src/promotion.py
+++ src/promotion.py
@@ -1,7 +1,10 @@
+from line import line
+
 class promotion:
 
-    def __init__():
+    def __init__(self, price):
         self.lines = []
+        self.price = price
     
     def add_line(self, SKU, quantity):
         self.lines.append(line(SKU, quantity))
--- src/tests/test_order.py
+++ src/tests/test_order.py
@@ -1,5 +1,6 @@
-from ..order import order
+from order import order
 from SKU import SKU
+from promotion import promotion
 
 def test_calculate_total():
     a = SKU('A', 10)
@@ -10,3 +11,16 @@ def test_calculate_total():
     o.add_line(b, 2)
 
     assert o.calculate_total() == 90
+
+def test_calculate_total_with_promotions():
+    a = SKU('A', 10)
+    b = SKU('B', 30)
+
+    o = order()
+    o.add_line(a, 3)
+    o.add_line(b, 2)
+
+    p = promotion(20)
+    p.add_line(a, 3)
+
+    assert o.calculate_total() == 80
